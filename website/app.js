6b3053ad6f00348f68c84a62652209f3efed5d48
ts2396 <48188551+ts2396@users.noreply.github.com>
2020-03-25 14:37:02 -0400

    commit

--- website/app.js
+++ website/app.js
@@ -2,8 +2,7 @@
 // Create a new date instance dynamically with JS //
 ////////////////////////////////////////////////////
 const d = new Date();
-const newDate = (d.getMonth() + 1) + '.' + d.getDate() + '.' + d.getFullYear();
-
+const newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
 
 ////////////////////////////////////////////
 // Personal API Key for OpenWeatherMap API//
@@ -22,75 +21,74 @@ document.getElementById('generate').addEventListener('click', performAction);
 
 function performAction(e) {
   const feelText = document.getElementById('feelings').value;
-  getApiData(baseURL, document.getElementById('zip').value, apiKey)
-    .then(function (JTempData) {
+  getApiData(baseURL, document.getElementById('zip').value, apiKey).then(
+    function(JTempData) {
       postData('./add', {
         temperature: JTempData,
         date: newDate,
         feelings: feelText
       });
       updateAPIData();
-    })
+    }
+  );
 }
-
-
-
 
 /* Function to GET Web API Data*/
 const getApiData = async (baseURL, zip, apiKey) => {
-  const response = await fetch(baseURL + zip + "&units=imperial" + apiKey);
+  const response = await fetch(baseURL + zip + apiKey);
   try {
     const webData = await response.json();
     JTempData = webData.main.temp;
-    return JTempData
+    return JTempData;
   } catch (error) {
-    console.log("error", error);
+    console.log('error', error);
   }
-}
+};
 
 /* Function to GET Project Data */
 const retrieveData = async (url = '') => {
   const request = await fetch(url);
   try {
     // Transform into JSON
-    const allData = await request.json()
-    return allData
+    const allData = await request.json();
+    return allData;
   } catch (error) {
-    console.log("error", error);
+    console.log('error', error);
     // appropriately handle the error
   }
 };
 
 /* Function to POST data */
 const postData = async (url = '', data = {}) => {
-
   const response = await fetch(url, {
     method: 'POST',
     credentials: 'same-origin',
     headers: {
-      "Content-Type": "application/json;charset=UTF-8",
+      'Content-Type': 'application/json;charset=UTF-8'
     },
-    body: JSON.stringify(data),
+    body: JSON.stringify(data)
   });
 
   try {
     const newData = await response.json();
     return newData;
   } catch (error) {
-    console.log("error", error);
+    console.log('error', error);
   }
 };
 
 /* Update UI */
 const updateAPIData = async () => {
-  const request = await fetch('/all')
+  const request = await fetch('/all');
   try {
-    const allData = await request.json()
+    const allData = await request.json();
     const newAPIData = allData[allData.length - 1];
     document.getElementById('date').innerHTML = 'Date: ' + newAPIData.date;
-    document.getElementById('temp').innerHTML = 'Temperature: ' + newAPIData.temperature + ' &#8457;';
-    document.getElementById('content').innerHTML = 'Feelings: ' + newAPIData.feelings;
+    document.getElementById('temp').innerHTML =
+      'Temperature: ' + newAPIData.temperature + ' &#8457;';
+    document.getElementById('content').innerHTML =
+      'Feelings: ' + newAPIData.feelings;
   } catch (error) {
-    console.log("error", error)
+    console.log('error', error);
   }
-}
+};
