b6abb6107a3dbea798497600a4217e3d1ca02814
ts2396 <48188551+ts2396@users.noreply.github.com>
2020-03-25 13:29:28 -0400

    commit

--- website/app.js
+++ website/app.js
@@ -10,7 +10,7 @@ const newDate = (d.getMonth() + 1) + '.' + d.getDate() + '.' + d.getFullYear();
 ////////////////////////////////////////////
 const baseURL =
   'https://api.openweathermap.org/data/2.5/weather?units=imperial&zip=';
-const apiKey = 'ce58fc8133b676ecc82cb35306506d86';
+const apiKey = '&appid=ce58fc8133b676ecc82cb35306506d86';
 
 // http://api.openweathermap.org/data/2.5/weather?q=us&APPID=ce58fc8133b676ecc82cb35306506d86
 
@@ -18,33 +18,28 @@ const apiKey = 'ce58fc8133b676ecc82cb35306506d86';
 // Event listener to add function to existing HTML DOM element//
 ////////////////////////////////////////////////////////////////
 
-/* Function called by event listener */
-
 document.getElementById('generate').addEventListener('click', performAction);
 
 function performAction(e) {
-  e.preventDefault();
-  const feelingText = document.getElementById('feelings').value;
-  getWeather(baseURL, document.getElementById('zip').value, apiKey).then(
-    function (newTemperature) {
+  const feelText = document.getElementById('feelings').value;
+  getApiData(baseURL, document.getElementById('zip').value, apiKey)
+    .then(function (tempData) {
       postData('/add', {
-        temperature: newTemperature,
+        temperature: JTempData,
         date: newDate,
-        feelings: feelingText
+        feelings: feelText
       });
-      updateUI();
-    }
-  );
+      updateAPIData();
+    })
 }
 
 /* Function to GET Web API Data*/
-
-const getWeather = async (baseURL, zip, apiKey) => {
-  const res = await fetch(baseURL + zip + ',&appid=' + apiKey);
+const getApiData = async (baseURL, zip, apiKey) => {
+  const response = await fetch(baseURL + zip + apiKey);
   try {
-    const webData = await res.json();
-    newTemperature = webData.main.temp;
-    return newTemperature
+    const webData = await response.json();
+    JTempData = webData.main.temp;
+    return JTempData
   } catch (error) {
     console.log("error", error);
   }
@@ -52,10 +47,11 @@ const getWeather = async (baseURL, zip, apiKey) => {
 
 /* Function to GET Project Data */
 const retrieveData = async (url = '') => {
-  const req = await fetch(url);
+  const request = await fetch(url);
   try {
-    const reqData = await req.json();
-    return reqData;
+    // Transform into JSON
+    const allData = await request.json()
+    return allData
   } catch (error) {
     console.log("error", error);
     // appropriately handle the error
@@ -64,13 +60,15 @@ const retrieveData = async (url = '') => {
 
 /* Function to POST data */
 const postData = async (url = '', data = {}) => {
+
   const response = await fetch(url, {
     method: 'POST',
+    mode: 'cors',
     credentials: 'same-origin',
     headers: {
-      "Content-Type": "application/json;charset=UTF-8"
+      "Content-Type": "application/json;charset=UTF-8",
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   });
 
   try {
@@ -81,20 +79,16 @@ const postData = async (url = '', data = {}) => {
   }
 };
 
-
-let updateUI = async () => {
-  const reqPost = await fetch('/all');
+/* Update UI */
+const updateAPIData = async () => {
+  const request = await fetch('/all')
   try {
-    const allData = await reqPost.json();
-    console.log(allData);
-    const newLocal = 1;
-    const newEntry = allData[allData.length - newLocal];
-    document.getElementById('date').innerHTML = 'Date: ' + newEntry.date;
-    document.getElementById('temp').innerHTML =
-      'Temperature: ' + newEntry.temperature + ' &#8457;';
-    document.getElementById('content').innerHTML =
-      'Feelings: ' + newEntry.feelings;
+    const allData = await request.json()
+    const newAPIData = allData[allData.length - 1];
+    document.getElementById('date').innerHTML = 'Date: ' + newAPIData.date;
+    document.getElementById('temp').innerHTML = 'Temperature: ' + newAPIData.temperature + ' &#8457;';
+    document.getElementById('content').innerHTML = 'Feelings: ' + newAPIData.feelings;
   } catch (error) {
-    console.log("error", error);
+    console.log("error", error)
   }
-};
+}
