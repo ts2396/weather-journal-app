7d1338b79758bafcabe0c7b3a9769d58d18190af
ts2396 <48188551+ts2396@users.noreply.github.com>
2020-04-06 18:19:55 -0400

    commit

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors());
 /* Initialize the main project folder*/
 app.use(express.static('website'));
 
-const port = 3000;
+const port = 8080;
 /* Spin up the server*/
 const server = app.listen(port, listening);
 function listening() {
